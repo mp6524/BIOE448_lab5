f1a3f1798a2bdc08652ad724a414f869fd5e78ba
unknown <mp65@rice.edu>
2024-02-13 15:12:15 -0600

    Added code

--- lab5/lab5.ino
+++ lab5/lab5.ino
@@ -1,5 +1,10 @@
+#include <ArduinoBLE.h>
+BLEService newService("180A"); // creating the service
+BLEByteCharacteristic readChar("2A57", BLERead);
+BLEByteCharacteristic writeChar("2A58", BLEWrite);
+
 const int trigPin = 8;
-const int echoPin = 120;
+const int echoPin = 10;
 long duration;
 int distanceCm, distanceInch;
 
@@ -11,6 +16,25 @@ void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   Serial.begin(9600);
+
+  while(!Serial);
+  if (!BLE.begin()){
+  Serial.println("Waiting for ArduinoBLE");
+  while(1);
+  }
+
+  BLE.setDeviceName("Your names here");
+  BLE.setAdvertisedService(newService);
+  newService.addCharacteristic(readChar);
+  newService.addCharacteristic(writeChar);
+  BLE.addService(newService);
+
+  readChar.writeValue(0);
+  writeChar.writeValue(0);
+
+  BLE.advertise();
+  Serial.println("Bluetooth device active");
+
 }
 
 void loop() {
