8ed7c31e591d12690dc2b9d5167419783b871a2b
unknown <mp65@rice.edu>
2024-02-13 14:47:31 -0600

    Initial commit

---
+++ lab5/lab5.ino
@@ -0,0 +1,35 @@
+const int trigPin = 8;
+const int echoPin = 120;
+long duration;
+int distanceCm, distanceInch;
+
+double R1 = 0.0343/2;
+double R2 = (0.0343/2.54)/2;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+
+  distanceCm = duration * R1;
+  distanceInch = duration * R2;
+  Serial.print("Distance: ");
+  Serial.print(distanceCm);
+  Serial.print(" cm/");
+  Serial.print(distanceInch);
+  Serial.println(" in");
+  delay(1000);
+
+}
